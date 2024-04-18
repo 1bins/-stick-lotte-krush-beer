22f9b94837f41c1659f40dabcc919cbaab9f2b42
서원빈 <1bins@kakao.com>
2024-04-18 11:45:57 +0900

    🩹 알파, 베타, 감마 값으로 조건식 수정

--- assets/js/beer.js
+++ assets/js/beer.js
@@ -13,27 +13,42 @@ function handleOrientation(event) {
 // 좌우로 기울일 때 마다 발생하는 이벤트
 function updateFieldIfNotNull(alpha, beta, gamma){
     if (gamma != null){
+        // 감마 절대값
         let absGamma = Math.abs(gamma);
+        let absBeta = Math.abs(beta);
 
-        if(gamma == 0){
-            $('#beer').addClass('paused')
-            gsap.to('.beer-wrap', {rotation: 0})
+        const leftSide = (alpha >= 180 && alpha <= 360 && absBeta >= 90 && absBeta <= 180 && gamma > 0) || 
+                        (alpha >= 0 && alpha < 180 && absBeta >= 0 && absBeta < 90 && gamma < 0);
 
-        }else if(gamma > -45 && gamma < 45){
-            $('#beer').addClass('paused')
-            gsap.to('.beer-wrap', {rotation: `${gamma * -0.4}`})
+        const rightSide = (alpha >= 0 && alpha < 180 && absBeta >= 90 && absBeta <= 180 && gamma < 0) || 
+                        (alpha >= 180 && alpha <= 360 && absBeta >= 0 && absBeta < 90 && gamma > 0);
+
+        let tl = gsap.timeline();
+
+        // 감마가 0일 경우
+        if (gamma === 0) {
+            $('#beer').addClass('paused');
+            tl.to('.beer-wrap', {rotation: 0});
         }
-        else {
-            $('#beer').removeClass('paused')
-            // $('#beer').addClass('active')
-
-            if(absGamma > 60){
-                // $('.beer-wrap').css({'transform': `rotate(30deg)`})
-                gsap.to('.beer-wrap', {rotation: 24, duration: 1.5})
-            }else {
-                gsap.to('.beer-wrap', {rotation: `${gamma * -0.4}`})
+        // 감마가 -60와 60사이
+        else if (gamma > -60 && gamma < 60) {
+            $('#beer').addClass('paused');
+            if (leftSide) {
+                tl.to('.beer-wrap', {rotation: `${absGamma * 0.4}`});
+            } else if (rightSide) {
+                tl.to('.beer-wrap', {rotation: `${absGamma * -0.4}`});
             }
         }
+        // 그 외의 경우
+        else {
+            $('#beer').removeClass('paused');
+            if (leftSide) {
+                tl.to('.beer-wrap', {rotation: 24});
+            } else if (rightSide) {
+                tl.to('.beer-wrap', {rotation: -24});
+            }
+        }
+
         
     }
 
